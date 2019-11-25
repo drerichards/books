b38a4bce65eaf4a78e21dd0fe470b2ce65bf60bd
A Richardson <g.andrerichards@gmail.com>
2019-11-25 04:55:54 -0500

    sequelize after err

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+// const db = require('./util/database');
 const sequelize = require('./util/database');
 
 const app = express();
@@ -13,6 +14,10 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+// db.execute('SELECT * FROM products')
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err))
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -30,3 +35,4 @@ sequelize.sync()
     app.listen(3000);
   })
   .catch(err => console.log(err))
+  // app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,8 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'mysql', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
 
 module.exports = sequelize;
+
+// const mysql = require('mysql2')
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: 'nodecomplete'
+// })
+
+// module.exports = pool.promise()
